763e197c560c8fb775923c46c7db02cf668d9de8
Shivam Saboo <201652025@iiitvadodara.ac.in>
2017-10-07 23:21:21 +0530

    Update sent_mail.py

--- sent_mail.py
+++ sent_mail.py
@@ -4,18 +4,69 @@ import smtplib
 from email.utils import parseaddr
 import time
 
-def send_mail(text_plain):
-    server = smtplib.SMTP('smtp.gmail.com:25',587)
-    server.starttls()
-    server.ehlo()
-    server.login("complaints.iiitv@gmail.com", "Pass123!@#")
-    server.connect("smtp.gmail.com:465")
-    server.sendmail("complaints.iiitv@gmail.com", "201651018@iiitv.ac.in", text_plain)
-    server.quit()
-
-def main():
-    send_mail_mail("Vande Matram")
 
 
-if __name__ == '__main__':
-    main()
+def check_new_mail():
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login('complaints.iiitv@gmail.com', 'Pass123!@#')
+    mail.list()
+    # Out: list of "folders" aka labels in gmail.
+    mail.select("inbox") # connect to inbox.
+
+    result, data = mail.uid('search', None, "UnSeen") # search and return uids instead
+
+    if len(data[0].split()) != 0:
+
+        for emailone in data[0].split():
+            latest_email_uid = emailone
+            result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+            raw_email = (data[0][1]).decode('utf-8')
+
+            message = email.message_from_string(raw_email)
+            text_plain = None
+            text_html = None
+
+            for part in message.walk():
+                if part.get_content_type() == 'text/plain' and text_plain is None:
+                    text_plain = part.get_payload()
+                if part.get_content_type() == 'text/html' and text_html is None:
+                    text_html = part.get_payload()
+
+            to = parseaddr(message.get('To'))[1]
+            From = parseaddr(message.get('From'))[1]
+            subject = (message.get('Subject'))
+
+            
+            
+            #Add the code for fetching tag frommm watson here
+            send_mail("ADD THE EMAIL ACCORDING TO TAG", subject,  text_plain)
+
+
+
+
+def send_mail(to_id, subject, body):
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    from_id = 'complaints.iiitv@gmail.com'
+    from_password = 'Pass123!@#'
+
+    msg = MIMEMultipart()
+    msg['From'] = from_id
+    msg['To'] = to_id
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body))
+
+    mailServer = smtplib.SMTP_SSL('smtp.gmail.com:465')
+
+    mailServer.login(from_id, from_password)
+    mailServer.sendmail(from_id, to_id, msg.as_string())
+    mailServer.close()
+
+while 1:
+    check_new_mail()
+
+
+
+
